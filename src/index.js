fa634d62acc99001d5bbb6f449cbb4d98307099a
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-08-23 16:50:07 -0400

    commit tree 15682aaf63536ecaf17c01002a9a2a4c13e3338d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const dogBreedsUl = document.getElementById('dog-breeds')
+const dogBreedsCollection = [];
+const breedDropdown = document.getElementById('breed-dropdown')
+
+function dataFetcher(url, pageSetupFunc) {   
+  fetch(url)
+    .then(response => response.json())
+    .then(data => pageSetupFunc(data));
+};
+
+function fetchImg(data){
+  data.message.forEach( img => {
+    const image = document.createElement('img');
+    image.src  = img;
+    document.getElementById('dog-image-container').appendChild(image);
+  });
+};
+
+dataFetcher(imgUrl, fetchImg);
+
+function fetchBreedInfo(data) {
+  const dogBreeds = Object.keys(data.message)
+  dogBreeds.forEach( breed => {
+    dogBreedsUl.innerHTML += `<li> ${breed} </li>`;
+    dogBreedsCollection.push(breed);
+  });
+};
+
+dataFetcher(breedUrl, fetchBreedInfo)
+
+// Event Listener
+breedDropdown.addEventListener('change', event => {
+  const breedsFiltered = dogBreedsCollection.filter(a => a[0] == event.target.value)
+  dogBreedsUl.innerHTML = '';
+  breedsFiltered.forEach( breed => dogBreedsUl.innerHTML += `<li> ${breed} </li>` );
+});
+
+});
